26ad823c07bc0d6872aba6e8bf549923468cf060
DEBORAHVIANAVIEIRAMENDONCA <31224361+DEBORAHVIANAVIEIRAMENDONCA@users.noreply.github.com>
2019-05-28 14:42:05 -0300

    Add files via upload

--- src/BotaoEscolha/botaoEscolha.js
+++ src/BotaoEscolha/botaoEscolha.js
@@ -11,10 +11,10 @@ class BotaoEscolha extends Component {
       <div className="book-shelf-changer">
         <select value={book.shelf} onChange={e => updateBook(book, e.target.value)}>
           <option value="Move to" disabled>Move to</option>
+          <option value="none">None</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
           <option value="read">Read</option>
-          <option value="none">None</option>
         </select>
       </div>
     );
