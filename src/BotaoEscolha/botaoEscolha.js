de2687639acbbfc59679f99dc82f1cf8b11b7336
DEBORAHVIANAVIEIRAMENDONCA <31224361+DEBORAHVIANAVIEIRAMENDONCA@users.noreply.github.com>
2019-05-29 22:06:29 -0300

    Add files via upload

--- src/BotaoEscolha/botaoEscolha.js
+++ src/BotaoEscolha/botaoEscolha.js
@@ -5,11 +5,12 @@ class BotaoEscolha extends Component {
 
 
   render() {
+    this.props.book != null ? console.log(this.props.book.title, this.props.book.shelf) : console.log("null");
     let prateleira = this.props.shelf;
     const { book, updateBook } = this.props; //desconstrução
     return (
       <div className="book-shelf-changer">
-        <select value={book.shelf} onChange={e => updateBook(book, e.target.value)}>
+        <select value={book.shelf != null ? book.shelf : "none"} onChange={e => updateBook(book, e.target.value)}>
           <option value="Move to" disabled>Move to</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
